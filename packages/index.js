358ec9585d13229558948854ca6b91abe83b814e
Alessia Sannazzaro <alessia@codeandwander.com>
2025-12-02 13:25:06 +0000

    Update index.js
    Added Inline SVG package

--- packages/index.js
+++ packages/index.js
@@ -2,24 +2,30 @@
 // Version: 1.0.1
 
 import { BLX_TOC } from './toc/index.js';
+import './inline-svg/index.js';
 // future imports:
 // import { BLX_SLIDER } from './slider/index.js';
 // import { BLX_ACCORDION } from './accordion/index.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  // TOC
-  if (document.querySelector('[blx-el="toc"]')) {
-    BLX_TOC();
-  }
+// TOC
+if (document.querySelector('[blx-el="toc"]')) {
+  BLX_TOC();
+}
 
-  // SLIDER (example)
-  // if (document.querySelector('[blx-el="slider"]')) {
-  //   BLX_SLIDER();
-  // }
+// Inline SVG
+if (document.querySelector('[blx-el="inline-svg"]')) {
+window.BLX?.inlineSVG?.();
+}
 
-  // ACCORDION (example)
-  // if (document.querySelector('[blx-el="accordion"]')) {
-  //   BLX_ACCORDION();
-  // }
+// SLIDER (example)
+// if (document.querySelector('[blx-el="slider"]')) {
+//   BLX_SLIDER();
+// }
+
+// ACCORDION (example)
+// if (document.querySelector('[blx-el="accordion"]')) {
+//   BLX_ACCORDION();
+// }
 });
